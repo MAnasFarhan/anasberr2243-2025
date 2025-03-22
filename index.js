0a3ab016e71a8a3caea2cf83d0c1f25fa2f2ab9a
ANAS <b122410326@student.utem.edu.my>
2025-03-22 20:00:03 +0800

    Edit index

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ async function main(){
         const collection = db.collection("users");
 
         //Insert a document
-        await collection.insertOne({ name: "Alice", age: 25});
+        await collection.insertOne({ name: "Anas", age: 23});
         console.log("Document inserted!");
         
         //Query the document
-        const result = await collection.findOne({ name: "Alice" });
+        const result = await collection.findOne({ name: "Anas" });
         console.log("Query result", result);
     } catch(err) {
         console.error("Error:", err);
