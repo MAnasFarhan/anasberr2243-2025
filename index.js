1462260844776cdb0014c8b7cccd1c7ec912b73d
MUHAMMAD ANAS FARHAN <b122410326@student.utem.edu.my>
2025-04-03 10:09:08 +0800

    W2-1 EDIT

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 const { MongoClient } = require('mongodb');
 
+const drivers = [
+    {
+        name: "John Doe",
+        vehicleType: "Sedan",
+        isAvailable: true,
+        rating: 4.8
+    },
+    {
+        name: "Alice Smith",
+        vehicleType: "SUV",
+        isAvailable: false,
+        rating: 4.5
+    }
+];
+//show the data in the console
+console.log(drivers);
+
+//TODO: show the all the drivers name in the console
+
+//TODO: add additional driver to the driver array
 async function main(){
     //Replace <connection-string> with your MongoDB URI
     const uri = "mongodb://localhost:27017"
