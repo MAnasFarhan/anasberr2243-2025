60965a4929c25a10c2350ab8ee5f93f67cbe0097
MUHAMMAD ANAS FARHAN <b122410326@student.utem.edu.my>
2025-04-11 23:13:45 +0800

    index3 3

--- index3.js
+++ index3.js
@@ -35,3 +35,49 @@ app.get('/rider', async (req, res) => {
         res.status(500).json({ error: "Failed to fetch rides" });
     }
 });
+
+//POST /rides - create a new ride
+app.post('/rides', async (req, res) => {
+    try {
+        const result = await db.collection('rides').insertOne(req.body);
+        res.status(201).json({ id: result.insertedId });
+    } catch (err) {
+        res.status(400).json({ error: "Invalid ride data" });
+    }
+});
+
+//PATCH /rides/:id - Update ride status
+app.patch('/rides/:id', async (req, res) => {
+    try {
+        const result = await db.collection('rides').updateOne(
+            { _id: new ObjectId(req.params.id) },
+            { $set: { status: req.body.status } }
+        );
+        
+        if (result.modifiedCount === 0) {
+            return res.status(404).json({ error: "Ride not found" });
+        }
+        res.status(200).json({ updated: result.modifiedCount });
+
+    }   catch (err) {
+        //Handle invalid ID format or DB errors
+        res.status(400).json({ error: "Invalid ride ID or data" });
+    }
+});
+
+//DELETE /rides/:id - Cancel a ride
+app.delete('/rides/:id', async (req, res) => {
+    try{
+        const result = await db.collection('rides').deleteOne(
+            { _id: new ObjectId(req.params.id) }
+        );
+
+        if (result.deletedCount === 0) {
+            return res.status(404).json({ error: "Ride not found" });
+        }
+        res.status(200).json({ deleted: result.deleteCount });
+
+    }   catch (err) {
+        res.status(400).json({ error: "Invalid ride ID" });
+    }
+});
