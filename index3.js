899d4e26895265e0ce59072378f6d5bc094d0e1e
MUHAMMAD ANAS FARHAN <b122410326@student.utem.edu.my>
2025-04-11 22:56:07 +0800

    index3 2

--- index3.js
+++ index3.js
@@ -25,3 +25,13 @@ connectToMongoDB();
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
+
+// GET /rides - fetch all rides
+app.get('/rider', async (req, res) => {
+    try{
+        const rides = await db.collection('rides').find().toArray();
+        res.status(200).json(rides);
+    } catch (err) {
+        res.status(500).json({ error: "Failed to fetch rides" });
+    }
+});
